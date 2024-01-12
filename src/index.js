5e840b8048151749a264880f2a11421dd7bb88af
Md. Jahangir Alam <jahangir.2012@outlook.com>
2024-01-13 02:12:22 +0600

    20:18

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("server running on port 8080");
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants"
+
+( async () => {
+    try {
+       await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
+    } catch (error) {
+        console.error("ERROR :",error)
+        throw error
+    }
+})()
