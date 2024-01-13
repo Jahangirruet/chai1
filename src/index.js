6173f329f7d1685e926ed9ad211c300d0f8c6ac5
Md. Jahangir Alam <jahangir.2012@outlook.com>
2024-01-13 20:57:48 +0600

    connected

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants"
+//require('dotenv').config({path: './env'})
 
-( async () => {
-    try {
-       await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
-    } catch (error) {
-        console.error("ERROR :",error)
-        throw error
-    }
-})()
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB()
+
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", () => console.log("Error connecting", error));
+    throw error;
+  } catch (error) {
+    console.error("ERROR :", error);
+    throw error;
+  }
+})();
+*/
