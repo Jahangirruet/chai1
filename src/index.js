75af0e0582ffb948cd94cd21a73ad6fd1dc5e73e
Md. Jahangir Alam <jahangir.2012@outlook.com>
2024-01-19 23:54:07 +0600

    zzz

--- src/index.js
+++ src/index.js
@@ -2,22 +2,19 @@
 
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config({
-  path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
 
-/*
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", () => console.log("Error connecting", error));
-    throw error;
-  } catch (error) {
-    console.error("ERROR :", error);
-    throw error;
-  }
-})();
-*/
+  // .then(()=>{
+  //   app.listen(process.env.PORT||8000,()=>{
+  //     console.log("server listening on port: " + process.env.PORT);
+  //   });
+  // })
+  // .catch((err) => {
+  //   console.log("MONGO DB CONNECTION fAILD: ", err.message);
+  // });
